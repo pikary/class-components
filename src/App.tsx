7e6b16381b354d1f6b3da58b933982e72349e1b2
nurlan <baitassovnurlan1507@gmail.com>
2025-02-02 21:35:30 +0500

    feat: add base and reset styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import React, { Component } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 interface AppProps {}
@@ -23,30 +21,7 @@ class App extends Component<AppProps, AppState> {
   };
 
   render() {
-    return (
-      <>
-        <div>
-          <a href="https://vite.dev" target="_blank" rel="noreferrer">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank" rel="noreferrer">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
-        </div>
-        <h1>Vite + React</h1>
-        <div className="card">
-          <button onClick={this.handleIncrement}>
-            count is {this.state.count}
-          </button>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test HMR
-          </p>
-        </div>
-        <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
-        </p>
-      </>
-    );
+    return <></>;
   }
 }
 
